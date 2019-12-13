9dcd4026ebf1a0e74db33379c802c03097063b85
Chanikya <chanikyaprakash@gmail.com>
2019-12-13 22:57:42 +0530

    example is completed, we require angular-mocks.js to add support for testing controllers

--- spec/ControllersSpec.js
+++ spec/ControllersSpec.js
@@ -1,3 +1,31 @@
 describe("Prime",function(){
 	
+	beforeEach(function(){
+		// module(function($provide){
+		// 	$provide
+		// });
+
+		module('ControllerTestingDemoApp');
+	});
+	
+	var $controller;
+	var ctrlTesting;
+
+	beforeEach(inject(function(_$controller_){
+		$controller=_$controller_;
+		ctrlTesting=$controller('testController');
+		ctrlTesting.Number=10;
+	}));
+
+	it('It should be No Prime Number',function () {
+		ctrlTesting.checkNumber()
+		expect(ctrlTesting.NumberType).toBe('non prime number');
+	});
+
+	it('It should be Prime Number',function () {
+		ctrlTesting.Number=10;
+		ctrlTesting.checkNumber()
+		expect(ctrlTesting.NumberType).toBe('Prime number');
+	});
+
 });
